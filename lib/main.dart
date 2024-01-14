1168e20cc1606185292d657dbbbd16255248b24d
azizbeklive <otaboevlive@gmail.com>
2024-01-14 17:46:44 +0500

    splash and launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Future.delayed(const Duration(seconds: 1));
+  FlutterNativeSplash.remove();
+
   runApp(const MyApp());
 }
 
